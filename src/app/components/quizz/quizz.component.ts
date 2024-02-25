2994ed98c10ab15f910d0656658b863f94f6ace8
avalo-lnx <avalo.rf@gmail.com>
2024-02-24 20:57:51 -0400

    feat: :sparkles: Add logic and functionality to components

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
-  styleUrls: ['./quizz.component.css']
+  styleUrls: ['./quizz.component.css'],
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
+  title: string = '';
+  questions: any;
+  questionSelected: any;
+  answers: string[] = [];
+  answerSelected: string = '';
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
+  finished: boolean = true;
+  result: string = "";
 
+  constructor() {}
+
+  ngOnInit(): void {
+    if (quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
+      this.questions = quizz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  playerChoice(value: string) {
+    this.answers.push(value);
+    console.log(value);
+    this.nextStep()
+  }
+
+  async nextStep() {
+    this.questionIndex++;
+    if (this.questionIndex < this.questionMaxIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      await this.determineResult()
+      this.finished = true;
+    }
+  }
+
+  determineResult() {
+    const counts: any = {};
+    var higher: number = 0;
+    this.answers.forEach(x => {counts[x] = (counts[x] || 0) + 1} );
+    console.log(this.answers);
+    console.log(counts);
+    if (counts['A']> counts['B']) {
+      this.result = quizz_questions.results['A'];
+    } else {
+      this.result = quizz_questions.results['B'];
+    }
+
+  }
 }
